0abd076597d0569a7da198bc01deb7e371852712
suryanshsh <suryanshshrivastava@gmail.com>
2022-07-09 04:33:21 +0530

    environment variables setup + google api pom dependency set + Request parameters constants

---
+++ src/main/java/com/fampay/sortytsearch/constant/Constants.java
@@ -0,0 +1,16 @@
+package com.fampay.sortytsearch.constant;
+
+public final class Constants {
+    public static class YoutubeRequestConstants{
+        public static final String Q = "q";
+        public static final String PART = "part";
+        public static final String TYPE = "type";
+        public static final String VIDEO = "video";
+        public static final String PUB_AFTER = "publishedAfter";
+        public static final String PUB_BEFORE = "publishedBefore";
+        public static final String ORDER = "order";
+        public static final String DATE = "date";
+        public static final String SNIPPET = "snippet";
+        public static final String KEY = "key";
+    }
+}
