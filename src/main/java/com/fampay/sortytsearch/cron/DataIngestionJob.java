533a578eb34e9bbadd4142ae5ce8ed5c40b25179
suryanshsh <suryanshshrivastava@gmail.com>
2022-07-10 14:55:59 +0530

    Enabling CRON scheduling

--- src/main/java/com/fampay/sortytsearch/cron/DataIngestionJob.java
+++ src/main/java/com/fampay/sortytsearch/cron/DataIngestionJob.java
@@ -2,14 +2,16 @@ package com.fampay.sortytsearch.cron;
 
 import com.fampay.sortytsearch.service.SearchResultIngestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import static com.fampay.sortytsearch.constant.Constants.DelayConstants.FIXED_DELAY;
 
 @Component
+@EnableScheduling
 public class DataIngestionJob {
-    private final SearchResultIngestionService ingestionService;
+    private SearchResultIngestionService ingestionService;
     @Autowired
     public DataIngestionJob(SearchResultIngestionService _searchResultIngestionService){
         ingestionService = _searchResultIngestionService;
